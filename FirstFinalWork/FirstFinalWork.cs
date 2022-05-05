8a667f37289b6348651f4940f733e18ce7235b9a
BigBadMuttuH <bigbadantonio@gmail.com>
2022-05-05 13:11:38 +0300

    update (отсалось нарисовать алгоритм :) )

--- FirstFinalWork/FirstFinalWork.cs
+++ FirstFinalWork/FirstFinalWork.cs
@@ -1,9 +1,19 @@
 ﻿public static class FirstFinalWork
 {
+    public static void Main(string[] args)
+    {
+        string[] s1 = {"hello", "2", "world", ":-)"}; //-> ["2", ":-)"]
+        string[] s2 = Get3CharsStrinArray(s1);
+        Console.WriteLine(String.Join(' ', s2));
+    }
+
+    /// <summary>
+    /// Возвращает массив элементов, длина которых меньше или равна 3 символам
+    /// </summary>
+    /// <param name="strArray"></param>
+    /// <returns> string[] </returns>
     public static string[] Get3CharsStrinArray(string[] strArray)
     {
-        string[] charArray = {};
-
-        return charArray;
+        return strArray.Where(x => x.Length < 4).ToArray();
     }
 }
